1b7bf4dfb20b894da92f7267dfa4681aed795740
Erik Nahmad <eriknahmad@gmail.com>
2022-05-05 23:27:15 -0700

    Create main.py

---
+++ Da Vinci/main.py
@@ -0,0 +1,11 @@
+# Project Name: Da Vinci
+# File Name: main.py
+# Author: Erik Nahmad
+# Date: March 26 2022
+# Purpose: Simple GUI to ask questions about Mathematics, Science, Technology, and History.
+
+from GUI import GUI
+
+root = GUI.Start()  # Window Start
+
+root.mainloop()  # Window Close
